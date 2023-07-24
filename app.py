97ba758556a2e89f81572677f456e789e16fc044
Appeltysken <Sorokin6203@yandex.ru>
2023-07-25 01:49:41 +0300

    💡 Add comments.
    - And also partially fixed the code.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import requests
 load_dotenv()
 
 app = Flask(__name__)
+
 app.secret_key = os.getenv("APP_SECRET_KEY")  # Секретный ключ (любой)
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")  # local host
 app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")  # root
@@ -26,8 +27,15 @@ app.jinja_env.globals.update(REDIRECTURI=REDIRECTURI)  # Редирект
 
 mysql = MySQL(app)
 
+
 class Database:
     def create_database():
+        """
+        Создает базу данных и таблицы, если их не существует.
+        Таблицы: accounts, users.
+        accounts - хранит информацию о зарегистрированных пользователях обычным способ.
+        users - хранит информацию о пользователях, которые авторизовались через VK.
+        """
         db_connection = MySQLdb.connect(
             host=os.getenv("MYSQL_HOST"),
             user=os.getenv("MYSQL_USER"),
@@ -51,136 +59,19 @@ class Database:
         )
         db_connection.close()
 
+
 Database.create_database()
 
-class User:
-    def __init__(self, username, password, email):
-        self.username = username
-        self.password = password
-        self.email = email
 
-    def save_to_database(self):
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        hashed_password = hashlib.sha1((self.password + app.secret_key).encode()).hexdigest()
-        cursor.execute(
-            "INSERT INTO accounts (username, password, email) VALUES (%s, %s, %s)",
-            (self.username, hashed_password, self.email),
-        )
-        mysql.connection.commit()
-
-
-class User_VK:
-    def __init__(self, user_id, username):
-        self.user_id = user_id
-        self.username = username
-
-    @staticmethod
-    def get_user_by_username_password(username, password):
-        hashed_password = hashlib.sha1((password + app.secret_key).encode()).hexdigest()
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            "SELECT * FROM accounts WHERE username = %s AND password = %s",
-            (username, hashed_password),
-        )
-        return cursor.fetchone()
-
-    @staticmethod
-    def get_user_by_user_id(user_id):
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
-        return cursor.fetchone()
-
-    def save_to_database(self):
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            "INSERT INTO users (user_id, username) VALUES (%s, %s)",
-            (self.user_id, self.username),
-        )
-        mysql.connection.commit()
-
-
-class WebPlayerApp:
-    def __init__(self, app):
-        self.app = app
-
-    def run(self):
-        self.app.run(debug=True)
-
-    def render_template(self, tmpl_name, **kwargs):
-        vk = False
-        user_id = session.get("user_id")
-        first_name = session.get("first_name")
-
-        if user_id:
-            vk = True
-        return render_template(tmpl_name, vk=vk, user_id=user_id, name=first_name, **kwargs)
-
-    def index(self):
-        return self.render_template("index.html")
-
-    def login_vk(self):
-        code = request.args.get("code")
-
-        response = requests.get(
-            f"https://oauth.vk.com/access_token?client_id={VKID}&redirect_uri={REDIRECTURI}&client_secret={VKSECRET}&code={code}"
-        )
-
-        access_token = response.json().get("access_token")
-        if not access_token:
-            return "Failed to retrieve access token"
-        params = {
-            "v": "5.101",
-            "fields": "uid,first_name,last_name",
-            "access_token": access_token,
-        }
-
-        get_info = requests.get(f"https://api.vk.com/method/users.get", params=params)
-        get_info = get_info.json().get("response")
-        if not get_info:
-            return "Failed to retrieve user info"
-        get_info = get_info[0]
-
-        session_data = {
-            "loggedin": True,
-            "user_id": get_info.get("id"),
-            "first_name": get_info.get("first_name"),
-            "last_name": get_info.get("last_name"),
-        }
-
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-
-        username = session_data["first_name"] + " " + session_data["last_name"]
-        user_id = session_data["user_id"]
-
-        def check_user_id(user_id):
-            try:
-                query = "SELECT * FROM users WHERE user_id = %s"
-                cursor.execute(query, (user_id,))
-                result = cursor.fetchone()
-                return bool(result)
-            except mysql.connection.Error as error:
-                print("Error:", error)
-                return False
-
-        if check_user_id(user_id):
-            session_data["username"] = username
-        else:
-            cursor.execute(
-                "INSERT INTO users (user_id, username) VALUES (%s, %s)",
-                (
-                    user_id,
-                    username,
-                ),
-            )
-            mysql.connection.commit()
-        session.update(session_data)
-        return redirect(url_for("home"))
-
-
-# Routes
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 @app.route("/musicwebplayer", methods=["GET", "POST"])
 def login():
+    """
+    Функция для обработки GET и POST запросов на главную страницу приложения.
+    Если метод запроса POST, то функция проверяет введенные данные пользователя и, если они верны,
+    создает сессию и перенаправляет пользователя на страницу home.
+    Если метод запроса GET, то функция возвращает шаблон страницы index.html.
+    """
     msg = ""
     if request.method == "POST":
         username = request.form.get("username")
@@ -194,12 +85,10 @@ def login():
             )
             account = cursor.fetchone()
             if account:
-                session_data = {
-                    "loggedin": True,
-                    "id": account["id"],
-                    "username": account["username"],
-                }
-                session.update(session_data)
+                session["loggedin"] = True
+                session["id"] = account["id"]
+                session["username"] = account["username"]
+                user = account
                 return redirect(url_for("home"))
             else:
                 msg = "Неверный логин/пароль."
@@ -208,12 +97,22 @@ def login():
 
 @app.route("/musicwebplayer/logout")
 def logout():
+    """
+    Функция для выхода пользователя из системы.
+    Она очищает сессию и перенаправляет пользователя на страницу авторизации.
+    """
     session.clear()
     return redirect(url_for("login"))
 
 
 @app.route("/musicwebplayer/register", methods=["GET", "POST"])
 def register():
+    """
+    Функция для обработки GET и POST запросов на страницу регистрации.
+    Если метод запроса POST, то функция проверяет введенные данные пользователя и, если они верны,
+    создает новый аккаунт в базе данных и перенаправляет пользователя на страницу авторизации.
+    Если метод запроса GET, то функция возвращает шаблон страницы register.html.
+    """
     msg = ""
     if request.method == "POST":
         username = request.form.get("username")
@@ -243,8 +142,14 @@ def register():
             msg = "Вы пропустили поле."
     return render_template("register.html", msg=msg)
 
+
 @app.route("/musicwebplayer/home")
 def home():
+    """
+    Функция для обработки GET запросов на домашнюю страницу.
+    Если пользователь авторизован, то функция возвращает шаблон страницы home.html с именем пользователя.
+    Если пользователь не авторизован, то функция перенаправляет пользователя на страницу авторизации.
+    """
     if "loggedin" in session:
         return render_template("home.html", username=session.get("username"))
     return redirect(url_for("login"))
@@ -252,6 +157,11 @@ def home():
 
 @app.route("/musicwebplayer/tracks")
 def get_tracks():
+    """
+    Функция для обработки GET запросов на страницу со списком треков.
+    Если пользователь авторизован, то функция возвращает шаблон страницы tracks.html с именем пользователя.
+    Если пользователь не авторизован, то функция перенаправляет пользователя на страницу авторизации.
+    """
     if "loggedin" in session:
         return render_template("tracks.html", username=session["username"])
     else:
@@ -260,12 +170,18 @@ def get_tracks():
 
 @app.route("/musicwebplayer/profile")
 def profile():
+    """
+    Функция для обработки GET запросов на страницу профиля пользователя.
+    Если пользователь авторизован, то функция возвращает шаблон страницы profile.html с данными пользователя.
+    Если пользователь не авторизован, то функция перенаправляет пользователя на страницу авторизации.
+    """
     if "loggedin" in session:
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
         if "user_id" in session:
-
-            cursor.execute("SELECT * FROM users WHERE user_id = %s", (session["user_id"],))
+            cursor.execute(
+                "SELECT * FROM users WHERE user_id = %s", (session["user_id"],)
+            )
             user_data = cursor.fetchone()
 
             if user_data:
@@ -276,10 +192,102 @@ def profile():
 
             if account:
                 return render_template("profile.html", account=account)
-
     return redirect(url_for("login"))
 
 
+@app.before_request
+def make_session_permanent():
+    """
+    Функция, которая устанавливает сессию как постоянную при каждом запросе.
+    """
+    session.permanent = True
+
+
+def template(tmpl_name, **kwargs):
+    """
+    Функция для рендеринга шаблона с передачей параметров.
+    :param tmpl_name: Имя шаблона.
+    :param kwargs: Передаваемые параметры.
+    :return: Рендеринг шаблона с передачей параметров.
+    """
+    vk = False
+    user_id = session.get("user_id")
+    first_name = session.get("first_name")
+
+    if user_id:
+        vk = True
+    return render_template(tmpl_name, vk=vk, user_id=user_id, name=first_name, **kwargs)
+
+
+@app.route("/login_vk")
+def login_vk():
+    """
+    Функция для обработки GET запросов на страницу авторизации через ВКонтакте.
+    Если пользователь авторизован, то функция перенаправляет пользователя на страницу home.
+    Если пользователь не авторизован, то функция перенаправляет пользователя на страницу авторизации ВКонтакте.
+    """
+    code = request.args.get("code")
+
+    response = requests.get(
+        f"https://oauth.vk.com/access_token?client_id={VKID}&redirect_uri={REDIRECTURI}&client_secret={VKSECRET}&code={code}"
+    )
+
+    params = {
+        "v": "5.101",
+        "fields": "uid,first_name,last_name,screen_name,sex,bdate,photo_big",
+        "access_token": response.json()["access_token"],
+    }
+
+    get_info = requests.get(f"https://api.vk.com/method/users.get", params=params)
+    get_info = get_info.json()["response"][0]
+
+    session["loggedin"] = True
+    session["user_id"] = get_info["id"]
+    session["first_name"] = get_info["first_name"]
+    session["last_name"] = get_info["last_name"]
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    username = session["first_name"] + " " + session["last_name"]
+    user_id = session["user_id"]
+
+    def check_user_id(user_id):
+        """
+        Функция для проверки наличия пользователя в базе данных.
+        :param user_id: ID пользователя.
+        :return: True, если пользователь существует в базе данных, иначе False.
+        """
+        try:
+            query = "SELECT * FROM users WHERE user_id = %s"
+            cursor.execute(query, (user_id,))
+
+            result = cursor.fetchone()
+
+            if result:
+                user_exists = True
+            else:
+                user_exists = False
+            print(user_exists)
+            return user_exists
+        except mysql.connection.Error as error:
+            print("Error:", error)
+            return False
+
+    if check_user_id(user_id):
+        session["username"] = username
+        return redirect(url_for("home"))
+    else:
+        cursor.execute(
+            "INSERT INTO users (user_id, username) VALUES (%s, %s)",
+            (
+                user_id,
+                username,
+            ),
+        )
+        mysql.connection.commit()
+    session["username"] = username
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
-    web_player_app = WebPlayerApp(app)
-    web_player_app.run()
+    app.run(debug=True)
